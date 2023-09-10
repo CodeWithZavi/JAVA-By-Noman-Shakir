53ff373e7b3196edecbe45c3ec191c383ad91e89
Noman shakir <nomanshaker2@gmail.com>
2023-09-10 14:33:36 +0500

    //

--- JAVA BASICS BY NOMAN SHAKIR/src/CarShowroom.java
+++ JAVA BASICS BY NOMAN SHAKIR/src/CarShowroom.java
@@ -4,7 +4,26 @@ public class CarShowroom {
         
           System.out.println("Welcome to Sardar's car Showroom");
 
-            
+          String car1 = "Honda Civic";
+          int prize1 = 20000;
+           String car2 = "luxes 570";
+          int prize2 = 150000;
+           String car3 = "Toyta V8";
+          int prize3 = 70000;
+
+          System.out.println(car1);
+          System.out.println(prize1);
+
+          System.out.println();
+
+          System.out.println(car2);
+          System.out.println(prize2);
+
+           System.out.println();
+          System.out.println(car3);
+          System.out.println(prize3);
+          
+          
            
      }
 }
