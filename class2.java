5fe0b4970602df624e925c1d88cf83131d141995
Noman shakir <nomanshaker2@gmail.com>
2023-09-17 18:49:54 +0500

    //

--- class2.java
+++ class2.java
@@ -1,5 +1,33 @@
-public class class2 {
-    
+ class A {
+      
+    int x;
+    String b;
+      A(int a,String c)
+      {
+          x = a;
+          b = c;
 
+          System.out.println(x+" "+c);
+      }
+
+
+      A(A ref)
+      {
+          int q = ref.x;
+          String p = ref.b;
+          // i am using print function
+          System.out.printf("age is %d and name is %s",q,p);
+          
+      }
+ }
+ class class2 {
+    
+   public static void main(String[] args) {
+    
+      A ref = new A(12, "noman");
+      new A(ref);  // it copy all data from 1st object
+
+     
+   }
          
 }
