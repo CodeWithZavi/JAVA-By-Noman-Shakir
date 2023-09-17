6dd693b3f7df883071be0abe7b0e48fdaff93a3b
Noman shakir <nomanshaker2@gmail.com>
2023-09-17 13:45:36 +0500

    //

---
+++ Java Class and object/printfunction.java
@@ -0,0 +1,12 @@
+public class printfunction {
+    
+      public static void main(String arg[])
+      {
+          
+           int a = 12;
+           int b = 23;
+
+           System.out.printf("%d + %d = %d ",a,b,a+b);
+      }
+}
+
