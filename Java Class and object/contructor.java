9605cfcae100b8df062e8761ae69e8f5a60a171a
Noman shakir <nomanshaker2@gmail.com>
2023-09-17 13:58:42 +0500

    //

--- Java Class and object/contructor.java
+++ Java Class and object/contructor.java
@@ -1,7 +1,30 @@
-public class contructor {
-    
-       contructor()
+public class contructor{
+
+       int b;
+       String name;
+       contructor() // contructor (specail method) // u can display the value
+       { 
+            b = 0;
+            name = null;
+                
+               
+       }
+       void show()
        {
-            
+              System.out.println(b+" "+name);
+               
        }
 }
+
+       // make an another class for an object
+
+       class B{
+                public static void main(String[] args) {
+                     
+                       contructor ref = new contructor(); // it would call auotomatically
+
+              // and for void show :
+               ref.show();
+                }
+       }
+
