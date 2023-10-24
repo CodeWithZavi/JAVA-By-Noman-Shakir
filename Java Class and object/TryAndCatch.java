ee84abf9cc9e8b0cca4f3deb01f33d72032c5b75
Noman shakir <nomanshaker2@gmail.com>
2023-10-24 15:27:12 +0500

    /

---
+++ Java Class and object/TryAndCatch.java
@@ -0,0 +1,25 @@
+          // Number format Exception
+public class TryAndCatch {
+    
+   public static void main(String[] args) {
+          try{
+              int c = 12%0;  // yeh exception ha hamrea pass
+              System.out.println("c = "+c);
+          }catch(ArithmeticException n)  //
+          {
+             System.out.println("it cant be solve");
+          }
+
+          // Number format Exception
+           String s = "nomn";
+          try {
+                 int a = Integer.parseInt(s);
+                 System.out.println(a);
+
+          }catch(NumberFormatException n){
+              System.out.println("it cant be converted");
+          }
+          System.out.println("this is try and catch");
+   }
+
+}
